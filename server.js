f768868e8d99972fc919662c8b15031dc038f835
SHIHENG ZHANG <shawn.zsh@gmail.com>
2016-03-31 15:04:24 -0700

    Configured to pust on Openshift.

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 // Set up app and modules
 var express = require("express");
 var path = require('path');
+var http = require('http');
 var app = express();
+
+// Setup Server
+var server = http.createServer(app);
 
 // Use angular components in public folder
 app.use(express.static('public'));
@@ -12,6 +16,9 @@ app.get('/', function(req,res) {
 });
 
 // Set up access port
-var port = process.env.PORT || 7000;
-app.listen(port);
-console.log('Shit blows at: ' + port);
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+ 
+server.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", server_port " + server_port )
+});
